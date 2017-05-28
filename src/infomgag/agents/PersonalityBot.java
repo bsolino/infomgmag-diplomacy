f4c95ae1dc30aad06f494cb0b2dd5f28dc1d1a30
Maayan Shvo <maayan.shvo@gmail.com>
2017-05-28 16:08:38 +0200

    fix

--- src/infomgag/agents/PersonalityBot.java
+++ src/infomgag/agents/PersonalityBot.java
@@ -490,8 +490,217 @@ public class PersonalityBot extends Player{
 	}
 	
 	
+BasicDeal searchForNewDealToPropose(List<Power> myAllies){
+		
+		BasicDeal bestDeal = null;
+		Plan bestPlan = null;
+
+		//Get a copy of our list of current commitments.
+		ArrayList<BasicDeal> commitments = new ArrayList<BasicDeal>(this.confirmedDeals);
+		
+		//First, let's see what happens if we do not make any new commitments.
+		bestPlan = this.dbraneTactics.determineBestPlan(game, me, commitments, myAllies);
+		
+		//If our current commitments are already inconsistent then we certainly
+		// shouldn't make any more commitments.
+		if(bestPlan == null){
+			return null;
+		}
+		
+		//let's generate 10 random deals and pick the best one.
+		for(int i=0; i<10; i++){
+			
+			//generate a random deal.
+			BasicDeal randomDeal = generateRandomDeal();
+			
+			if(randomDeal == null){
+				continue;
+			}
+			
+			
+			//add it to the list containing our existing commitments so that dBraneTactics can determine a plan.
+			commitments.add(randomDeal);
+
+			
+			//Ask the D-Brane Tactical Module what it would do under these commitments.
+			Plan plan = this.dbraneTactics.determineBestPlan(game, me, commitments, myAllies);
+			
+			//Check if the returned plan is better than the best plan found so far.
+			if(plan != null && plan.getValue() > bestPlan.getValue()){
+				bestPlan = plan;
+				bestDeal = randomDeal;
+			}
+			
+			
+			//Remove the randomDeal from the list, for the next iteration.
+			commitments.remove(commitments.size()-1);
+			
+			//NOTE: the value returned by plan.getValue() represents the number of Supply Centers that the D-Brane Tactical Module
+			// expects to conquer in the current round under the given commitments.
+			//
+			// Of course, this is only a rough indication of which plan is truly the "best". After all, sometimes it is better
+			// not to try to conquer as many Supply Centers as you can directly, but rather organize your armies and only attack in a later
+			// stage.
+			// Therefore, you may want to implement your own algorithm to determine which plan is the best.
+			// You can call plan.getMyOrders() to retrieve the complete list of orders that D-Brane has chosen for you under the given commitments. 
+			
+		}
+		
+		
+		return bestDeal;
+		
+
+		
+	}
+	
+
+public BasicDeal generateRandomDeal(){
 	
 	
+	//Get the names of all the powers that are connected to the negotiation server (some players may be non-negotiating agents, so they are not connected.)
+	List<String> negotiatingPowers = negoClient.getRegisteredNames();
+	
+	//Make a copy of this list that only contains powers that are still alive.
+	// (A power is dead when it has lost all its armies and fleet)
+	List<Power> aliveNegotiatingPowers = new ArrayList<Power>(7);
+	for(String powerName : negotiatingPowers){
+		
+		Power negotiatingPower = game.getPower(powerName);
+		
+		if( ! game.isDead(negotiatingPower)){
+			aliveNegotiatingPowers.add(negotiatingPower);
+		}
+	}
+	
+	//if there are less than 2 negotiating powers left alive (only me), then it makes no sense to negotiate.
+	int numAliveNegoPowers = aliveNegotiatingPowers.size();
+	if(numAliveNegoPowers < 2){
+		return null;
+	}
+	
+	
+	
+	//Let's generate 3 random demilitarized zones.
+	List<DMZ> demilitarizedZones = new ArrayList<DMZ>(3);
+	for(int i=0; i<3; i++){
+		
+		//1. Create a list of powers
+		ArrayList<Power> powers = new ArrayList<Power>(2);
+		
+		//1a. add myself to the list
+		powers.add(me);
+		
+		//1b. add a random other power to the list.
+		Power randomPower = me;
+		while(randomPower.equals(me)){
+			
+			int numNegoPowers = aliveNegotiatingPowers.size();
+			randomPower = aliveNegotiatingPowers.get(random.nextInt(numNegoPowers));
+		}
+		powers.add(randomPower);
+		
+		//2. Create a list containing 3 random provinces.
+		ArrayList<Province> provinces = new ArrayList<Province>();
+		for(int j=0; j<3; j++){
+			int numProvinces = this.game.getProvinces().size();
+			Province randomProvince = this.game.getProvinces().get(random.nextInt(numProvinces));
+			provinces.add(randomProvince);
+		}
+		
+		
+		//This agent only generates deals for the current year and phase. 
+		// However, you can pick any year and phase here, as long as they do not lie in the past.
+		// (actually, you can also propose deals for rounds in the past, but it doesn't make any sense
+		//  since you obviously cannot obey such deals).
+		demilitarizedZones.add(new DMZ( game.getYear(), game.getPhase(), powers, provinces));
+
+	}
+	
+	
+	
+	
+	//let's generate 3 random OrderCommitments
+	List<OrderCommitment> randomOrderCommitments = new ArrayList<OrderCommitment>();
+	
+	
+	//get all units of the negotiating powers.
+	List<Region> units = new ArrayList<Region>();
+	for(Power power : aliveNegotiatingPowers){
+		units.addAll(power.getControlledRegions());
+	}
+	
+	
+	for(int i=0; i<3; i++){
+		
+		//Pick a random unit and remove it from the list
+		if(units.size() == 0){
+			break;
+		}
+		Region randomUnit = units.remove(random.nextInt(units.size()));
+		
+		//Get the corresponding power
+		Power power = game.getController(randomUnit);
+		
+		//Determine a list of potential destinations for the unit.
+		// a Region is a potential destination for a unit if it is adjacent to that unit (or it is the current location of the unit)
+		//  and the Province is not demilitarized for the Power controlling that unit.
+		List<Region> potentialDestinations = new ArrayList<Region>();
+		
+		//Create a list of adjacent regions, including the current location of the unit.
+		List<Region> adjacentRegions = new ArrayList<>(randomUnit.getAdjacentRegions());
+		adjacentRegions.add(randomUnit);
+		
+		for(Region adjacentRegion : adjacentRegions){
+			
+			Province adjacentProvince = adjacentRegion.getProvince();
+			
+			//Check that the adjacent Region is not demilitarized for the power controlling the unit.
+			boolean isDemilitarized = false;
+			for(DMZ dmz : demilitarizedZones){
+				if(dmz.getPowers().contains(power) && dmz.getProvinces().contains(adjacentProvince)){
+					isDemilitarized = true;
+					break;
+				}
+				
+			}
+			
+			//If it is not demilitarized, then we can add the region to the list of potential destinations.
+			if(!isDemilitarized){
+				potentialDestinations.add(adjacentRegion);
+			}
+		}
+		
+		
+		int numPotentialDestinations = potentialDestinations.size();
+		if(numPotentialDestinations > 0){
+			
+			Region randomDestination = potentialDestinations.get(random.nextInt(numPotentialDestinations));
+			
+			Order randomOrder;
+			if(randomDestination.equals(randomUnit)){
+				randomOrder = new HLDOrder(power, randomUnit);
+			}else{
+				randomOrder = new MTOOrder(power, randomUnit, randomDestination);
+			}
+			// Of course we could also propose random support orders, but we don't do that here.
+			
+			
+			//We only generate deals for the current year and phase. 
+			// However, you can pick any year and phase here, as long as they do not lie in the past.
+			// (actually, you can also propose deals for rounds in the past, but it doesn't make any sense
+			//  since you obviously cannot obey such deals).
+			randomOrderCommitments.add(new OrderCommitment(game.getYear(), game.getPhase(), randomOrder));
+		}
+		
+	}
+	
+	BasicDeal deal = new BasicDeal(randomOrderCommitments, demilitarizedZones);
+
+	
+	return deal;
+	
+}
+
 	public void negotiate(List<Power> myAllies, long negotiationDeadline) {
 		
 		//This loop repeats 2 steps. The first step is to handle any incoming messages, 
@@ -518,7 +727,8 @@ public class PersonalityBot extends Player{
 			//STEP 2:  try to find a proposal to make, and if we do find one, propose it.
 			BasicDeal newDealToPropose = null;
 			if(newDealToPropose == null){ //we only make one proposal per round, so we skip this if we have already proposed something.
-				newDealToPropose = decisionMaker.searchForADealToPropose();
+				//newDealToPropose = decisionMaker.searchForADealToPropose();
+				newDealToPropose = searchForNewDealToPropose(myAllies);
 				if(newDealToPropose != null){
 					
 					try {
@@ -552,7 +762,8 @@ public class PersonalityBot extends Player{
 		
 		decisionMaker.update(this.submittedOrders);
 		List<Power> recievers = game.getPowers();
-		this.negoClient.sendInformalMessage(recievers,decisionMaker.getPersonalityValues());
+		logger.logln(decisionMaker.getPersonalityValues(), true);
+		//this.negoClient.sendInformalMessage(recievers,decisionMaker.getPersonalityValues());
 		//To prevent games from taking too long, we automatically propose a draw after
 		// the FAL phase of the final year.
 		if((game.getYear() == finalYear && game.getPhase() == Phase.FAL) || game.getYear() > finalYear){
